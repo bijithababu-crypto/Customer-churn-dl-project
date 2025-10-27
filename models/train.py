a805e411f657c689ac09c92e33e0f3229d39c80f
bijithababu-crypto <bijitha.babu@arts.christuniversity.in>
2025-10-27 02:49:35 +0000

    Transform of the data

--- models/train.py
+++ models/train.py
@@ -6,3 +6,12 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.model import load_model
+
+df= pd.read.csv('data/Customer_churn_csv')
+x= df.drop(('CustomerId','Surname','Exited'),axis=1)
+y=df['Exited']
+
+X['Geography'] = LabelEncoder().fit_transform(X['Geography'])
+X['Gender'] = LabelEncoder().fit_transform(X['Gender'])
+
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=42)
